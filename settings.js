bf98aa459c62a7d0702bcc419da62d34c7a3d6db
hk777-5 <142346214+hk777-5@users.noreply.github.com>
2025-01-20 02:16:32 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
-global.bankname = "MONIEPOINT"
+global.bankname = "OPAY"
 
-global.banknumber = "8133729715"
+global.banknumber = "8089424814"
 
-global.bankowner = "FADARE"
+global.bankowner = "Henry"
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
